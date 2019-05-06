a5668c284e7544f57e6926dd68c203f14e09f18d
恭喜发财 <@PC-LZJ>
2019-05-06 16:09:35 +0800

    b

--- webapi/Enum/ResultCode.cs
+++ webapi/Enum/ResultCode.cs
@@ -26,41 +26,4 @@ namespace webapi.Enum
         [Display(Name = "请求失败")]
         Error = 500,
     }
-
-    public static partial class EnumExted
-    {
-        /// <summary>
-        /// 获取枚举字段的Display特性的名称
-        /// </summary>
-        /// <param name="e">枚举字段</param>
-        /// <returns></returns>
-        public static string GetFieldDisplay(this System.Enum e)
-        {
-            if (e == null)
-            {
-                return null;
-            }
-
-            var display = e.GetAttribute<DisplayAttribute>();
-            if (display == null)
-            {
-                return e.ToString();
-            }
-            return display.Name;
-        }
-
-        /// <summary>
-        /// 获取特性
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        public static T GetAttribute<T>(this System.Enum e) where T : class
-        {
-            var field = e.GetType().GetField(e.ToString());
-            var attribute = Attribute.GetCustomAttribute(field, typeof(T)) as T;
-            return attribute;
-        }
-    }
-
 }
